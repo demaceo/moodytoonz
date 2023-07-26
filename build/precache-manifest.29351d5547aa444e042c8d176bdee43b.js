dff05154b7cb0d65dfe1f1ceff73e2d8370cc959
demaceo <hdemaceo@gmail.com>
2023-07-26 13:02:14 -0600

    build

---
+++ build/precache-manifest.29351d5547aa444e042c8d176bdee43b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48c5d09921ea285576df",
+    "url": "/static/css/main.6825edbe.chunk.css"
+  },
+  {
+    "revision": "48c5d09921ea285576df",
+    "url": "/static/js/main.48c5d099.chunk.js"
+  },
+  {
+    "revision": "eed9962e296eed6eb667",
+    "url": "/static/js/1.eed9962e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18b9b618417dad15b613ac2a39db753e",
+    "url": "/index.html"
+  }
+];
