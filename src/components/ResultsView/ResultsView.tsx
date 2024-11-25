32d955716b3e312fd957ca9ff1de44a3dffeadeb
demaceo <hdemaceo@gmail.com>
2024-11-25 09:41:27 -0700

    provide unique keys and no duplicates for Results

--- src/components/ResultsView/ResultsView.tsx
+++ src/components/ResultsView/ResultsView.tsx
@@ -12,16 +12,25 @@ const ResultsView = ({
     document.title = `MoodyTunes - Results (${songResults.length})`;
   }, [songResults.length]);
 
-  const songs = songResults.map((song) => {
-    console.log(song)
+    const seenKeys = new Set();
+
+    // Filter out songs with duplicate keys
+    const uniqueSongs = songResults.filter((song) => {
+      if (seenKeys.has(song.id)) {
+        return false;
+      }
+      seenKeys.add(song.id); 
+      return true; 
+    });
+
+  const songs = uniqueSongs.map((song) => {
     return (
       <Result
-        key={song.id}
+        key={`${song.id}-${song.name}`}
         id={song.id}
         artist={song["artists"][0].name}
         name={song["name"]}
         releaseDate={song.album.release_date}
-        // genre={song.genre}
         url={song.external_urls["spotify"]}
         img={song.album.images[0]["url"]}
         favoriteSongs={favoriteSongs}
