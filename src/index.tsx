3154d2be4f4dab0d62d1c7098ed2eea9c88c0aba
demaceo <hdemaceo@gmail.com>
2025-01-04 14:30:05 -0700

    Spotify's recommendations API endpoint has been depricated

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
-// import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 // const router = <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>
@@ -18,10 +16,6 @@ root.render(
   </BrowserRouter>
 );
 
-// ReactDOM.render(
-//   router,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
